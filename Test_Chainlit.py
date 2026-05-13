74ab8b0f7fe598e9fe86dbceab09eb039f7777e1
Thanh Mai <mvtt.job@gmail.com>
2026-05-13 23:34:10 +0900

    reuse function def

--- Test_Chainlit.py
+++ Test_Chainlit.py
@@ -13,6 +13,7 @@ llm = ChatOllama(
 #global attribute pdf_text
 pdf_text = ""
 vectorstore = None
+chunks = 0
 
 embeddings = OllamaEmbeddings(
     model = 'nomic-embed-text'
@@ -42,11 +43,30 @@ def chunk_text(pdf_text):
 
     return chunks
 
+def called_prompt(question, k):
+    results = vectorstore.similarity_search(question, k = k)
+
+    context = "\n\n".join([
+        doc.page_content for doc in results
+    ])
+
+    prompt = f"""
+        Answer the question based only on the context below.
+
+        Context:
+        {context}
+
+        Question:
+        {question}
+
+        If the answer is not in the context, say you don't know.
+    """
+
+    return prompt
+
+
 @cl.on_message
 async def main(message: cl.Message):
-
-    global pdf_text
-    global vectorstore
 
     # Case 1: User uploads PDF
     if message.elements:
@@ -55,40 +75,22 @@ async def main(message: cl.Message):
 
                 pdf_text = parse_pdf(element.path)
                 chunks = chunk_text(pdf_text)
-
-                    # print("Number of chunks:", len(chunks))
-                    # print("First chunk:")
-                    # print(chunks[0])
-
-                print('chunks: ',len(chunks))
-
+                
                 if(chunks):
                     vectorstore = FAISS.from_texts(
                         chunks,
                         embedding= embeddings
                     )
 
+                    # set_chunks & vectorstore for reuse purposes
+                    cl.user_session.set('pdf_text', pdf_text)
+                    cl.user_session.set("chunks", chunks)
+                    cl.user_session.set("vectorstore", vectorstore)
+
                     # in cases where users upload PDF and yield questions simultaneously
                     if message.content.strip():
                         question = message.content
-
-                        results = vectorstore.similarity_search(question, k = len(chunks))
-
-                        context = "\n\n".join([
-                            doc.page_content for doc in results
-                        ])
-
-                        prompt = f"""
-                            Answer the question based only on the context below.
-
-                            Context:
-                            {context}
-
-                            Question:
-                            {message.content}
-
-                            If the answer is not in the context, say you don't know.
-                        """
+                        prompt = called_prompt(question, len(chunks))
 
                         response = llm.invoke(prompt)
 
@@ -103,35 +105,14 @@ async def main(message: cl.Message):
 
                 return  
 
-                        
-
     # Case 2: User asks a question
-    if vectorstore is None:
+    if vectorstore is None or chunks is None:
         await cl.Message(
             content = 'Please upload a PDF first'
         ).send()
         return
     
-    results = vectorstore.similarity_search(
-        message.content,
-        k=3
-    )
-
-    context = "\n\n".join([
-        doc.page_content for doc in results
-    ])
-
-    prompt = f"""
-        Answer the question based only on the context below.
-
-        Context:
-        {context}
-
-        Question:
-        {message.content}
-
-        If the answer is not in the context, say you don't know.
-        """
+    prompt = called_prompt(message.content, len(chunks))
 
     response = llm.invoke(prompt)
 
